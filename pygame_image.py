a4a4daded2a76caf5c380e9816fd3b99259b169e
yamashita <c0a2316126@edu.teu.ac.jp>
2024-11-19 17:21:58 +0900

    練習8 kannsei

--- pygame_image.py
+++ pygame_image.py
@@ -11,20 +11,38 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img,True,False)
-    kokaton_img = pg.image.load("fig/3.png")
-    kokaton_img = pg.transform.flip(kokaton_img,True,False)
-    kokaton_img = pg.transform.rotozoom(kokaton_img,10,1.0)
+    kk_img = pg.image.load("fig/3.png") 
+    kk_img = pg.transform.flip(kk_img, True, False) 
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300,200
+
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-    
+
+        Key_lst = pg.key.get_pressed()
+
+        if Key_lst[pg.K_UP]:
+            kk_rct.move_ip(0,-1)
+
+        if Key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0,1)
+
+        if Key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1,0)
+
+        if Key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(+1,0)
+        
         x = -(tmr%3200)
         screen.blit(bg_img, [x,0])
         screen.blit(bg_img2,[x+1600, 0])
         screen.blit(bg_img, [x+3200,0])
         screen.blit(bg_img2, [x+4800,0])
-        screen.blit(kokaton_img,[300,200])
+        #screen.blit(kk_img,[300,200])
+        screen.blit(kk_img,kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
