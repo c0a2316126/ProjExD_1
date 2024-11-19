b4a81952e7f89fc85596a5e5b93e9049c4d2336e
yamashita <c0a2316126@edu.teu.ac.jp>
2024-11-19 17:24:51 +0900

    演習１

--- pygame_image.py
+++ pygame_image.py
@@ -34,8 +34,10 @@ def main():
             kk_rct.move_ip(-1,0)
 
         if Key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+1,0)
-        
+            kk_rct.move_ip(+2,0)
+        kk_rct.move_ip(-1,0)
+
+
         x = -(tmr%3200)
         screen.blit(bg_img, [x,0])
         screen.blit(bg_img2,[x+1600, 0])
